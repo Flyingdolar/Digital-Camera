a871da259d9e8a9097757ba988cbb4a51711f5ee
翔元 <31057818+Flyingdolar@users.noreply.github.com>
2023-10-06 16:36:00 +0800

    feat: Finish hw2

--- hw2/main.py
+++ hw2/main.py
@@ -4,13 +4,69 @@ import os
 
 folderName = "subset"
 fileList = []
+mindist = 10000
+minfile = ""
+exposure = 0.18
+colorLevel = 256
+centWeight = [
+    [2, 0, 2, 0, 2],
+    [0, 2, 4, 2, 0],
+    [0, 4, 16, 4, 0],
+    [0, 2, 4, 2, 0],
+    [2, 1, 2, 1, 2],
+]
 
 # Use OS to scan all files in the folder
 for file in os.listdir(folderName):
     if file.endswith(".bmp"):
         fileList.append(file)
 
+print("Process Start")
+print("Using Central Weighting find the best image with exposure " + str(exposure))
+print("Reading files...")
+print("Total files: " + str(len(fileList)))
+
 # Print all files in the folder
 print("File List:")
 for file in fileList:
-    print(file)
+    img = cv.imread(folderName + "/" + file)  # Read the image
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)  # Change img from BGR to Gray
+    height, width = img.shape  # Get the size of the image
+
+    # Divide the image into 25 blocks, and calculate the mean of each block
+    block = [[0 for i in range(5)] for j in range(5)]
+    for i in range(5):
+        for j in range(5):
+            block[i][j] = np.mean(
+                img[
+                    int(height / 5 * i) : int(height / 5 * (i + 1)),
+                    int(width / 5 * j) : int(width / 5 * (j + 1)),
+                ]
+            )
+
+    # Calculate the dot product of the block and the central weight
+    dotProduct = 0
+    for i in range(5):
+        for j in range(5):
+            dotProduct += block[i][j] * centWeight[i][j]
+    dotProduct /= 64
+    dist = dotProduct - int(exposure * colorLevel)
+    print(
+        "Reading "
+        + file
+        + "..."
+        + "bright = "
+        + str(dotProduct)  # convert dotProduct to string
+        + ", dist = "
+        + str(dist)
+        + "..."
+    )
+    dist = abs(dist)
+    # Record if it is min-dist
+    if dist < mindist:
+        mindist = dist
+        minfile = file
+
+# Print the result
+print("Process Finished")
+print("The best image is " + minfile + " with distance " + str(mindist))
